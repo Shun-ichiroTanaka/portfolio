afbec1fba353c7923c42c8827ad4272eb696a94d
shun.ichiro.tanaka16@gmail.com <34049491+Shun-ichiroTanaka@users.noreply.github.com>
2019-10-18 14:31:30 +0900

    エラー修正

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,8 @@
-const functions = require("firebase-functions");
-const nodemailer = require("nodemailer");
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const adminEmail = functions.config().admin.email;
+const functions = require('firebase-functions');
 
-// 送信に使用するメールサーバーの設定
-const mailTransport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-        user: 'ealbalbs809@gmail.com',
-        pass: '88S11H33u44n66'
-  }
-});
-
-// 管理者用のメールテンプレート
-const adminContents = data => {
-  return `以下内容でホームページよりお問い合わせを受けました。
-お名前：
-${data.name}
-メールアドレス：
-${data.email}
-内容：
-${data.contents}
-`;
-};
-
-exports.sendMail = functions.https.onCall((data, context) => {
-  // メール設定
-  let adminMail = {
-    from: gmailEmail,
-    to: adminEmail,
-    subject: "ホームページお問い合わせ",
-    text: adminContents(data)
-  };
-
-  // 管理者へのメール送信
-  mailTransport.sendMail(adminMail, (err, info) => {
-    if (err) {
-      return console.error(`admin send failed. ${err}`);
-    }
-    return console.log("admin and user success");
-  });
-});
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
